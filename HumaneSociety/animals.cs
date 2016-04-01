225551e47a22eb9885f5abb864cf1b598444ca04
danFbach <quiktekrepair@gmail.com>
2016-04-01 12:59:18 -0500

    stubbed out most of what i need

---
+++ HumaneSociety/animals.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    public class animals
+    {
+        public string animalName;
+        public string breed;
+        public string foodType;
+        public double foodQty;
+        public double cageNumber;
+        public animals()
+        {
+        List<animals> animalInventory = new List<animals>();
+        }
+
+    }
+}
---
+++ HumaneSociety/userInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    public class userInterface
+    {
+        people people = new people();
+        humaneSociety modifyAnimals = new humaneSociety();
+    }
+}
